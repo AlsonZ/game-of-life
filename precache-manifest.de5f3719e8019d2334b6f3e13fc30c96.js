16b5b82a7aebe65c7d4f485e1131c419ce67bd67
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-02-27 17:44:32 +1100

    Updates

---
+++ precache-manifest.de5f3719e8019d2334b6f3e13fc30c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4828ca74ec352f41a0753ddb1e92c3d",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "817acddd0ee2fbad2ae4",
+    "url": "/game-of-life/static/css/main.2de2a80c.chunk.css"
+  },
+  {
+    "revision": "ac49868996b6e4044508",
+    "url": "/game-of-life/static/js/2.0eb82321.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/game-of-life/static/js/2.0eb82321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817acddd0ee2fbad2ae4",
+    "url": "/game-of-life/static/js/main.a6ae5ee7.chunk.js"
+  },
+  {
+    "revision": "345e64b5c07b20e5baa1",
+    "url": "/game-of-life/static/js/runtime-main.15c5fa34.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48452d6dd4451ff8e971cc7aeff68954.js"
+  "/game-of-life/precache-manifest.de5f3719e8019d2334b6f3e13fc30c96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-of-life/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
